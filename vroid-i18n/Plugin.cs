089380455989c8e00c81c6b04ba782af3ebadd07
rainbowflesh <catkingmagnumdemon@gmail.com>
2024-02-26 20:45:40 +0800

    init

---
+++ vroid-i18n/Plugin.cs
@@ -0,0 +1,18 @@
+﻿using BepInEx;
+using BepInEx.IL2CPP;
+
+namespace vroid_i18n
+{
+    [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+    public class Plugin : BasePlugin
+    {
+        public override void Load()
+        {
+            // Plugin startup logic
+            Log.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+        }
+    }
+}
+
+[BepInPlugin("org.bepinex.plugins.exampleplugin", "Example Plug-In", "1.0.0.0")]
+public class ExamplePlugin : BaseUnityPlugin { }
